97e99e3c36cfa69eec97fdec317428e40144789a
BadenMorgan <baden.david.morgan@gmail.com>
2016-04-14 04:33:04 +0200

    much improvement, many code
    *optimisation, bug fixes, etc....

--- HomeAuto/app/src/main/java/com/example/baden/HomeAuto/DynamicVariables.java
+++ HomeAuto/app/src/main/java/com/example/baden/HomeAuto/DynamicVariables.java
@@ -18,30 +18,9 @@ public class DynamicVariables {
     public static byte WINDOW2 = 0;
     //door  Mode boolean
     public static byte DOOR = 0;
-    //hours variable received from broker
-    public static byte hours = 0;
-    //minutes variable received from broker
-    public static byte minutes = 0;
-    //seconds variable received from broker
-    public static byte seconds = 0;
-    //day variable received from broker
-    public static byte day = 0;
-    //month variable received from broker
-    public static byte month = 0;
-    //year variable received from broker
-    public static byte year = 0;
-    //temperature part 1 variable received from broker
-    public static byte temperature1 = 0;
-    //temperature part 2 variable received from broker
-    public static byte temperature2 = 0;
-    //indicator byte for LED indicators
+    //LED variables
     public static byte LED = 0;
-    //last movement variables
-    public static byte lastsecond = 0;
-    public static byte lastminute = 0;
-    public static byte lasthour = 0;
-    public static byte lastday = 0;
-    public static byte lastmonth = 0;
+
 
 
 }
